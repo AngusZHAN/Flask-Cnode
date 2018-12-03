313471353650e24a0707341c20627241b6025e10
AngusZHAN <573585136@qq.com>
2018-12-03 19:24:22 +0800

    添加了发布主题功能

--- app/main/views.py
+++ app/main/views.py
@@ -1,13 +1,48 @@
 from . import main
-from flask import render_template
-
+from .. import db
+from ..models import (
+    User,
+    Topic,
+    Reply,
+)
+from flask import (
+    render_template,
+    redirect,
+    request,
+    url_for,
+    flash,
+    session,
+)
+from flask_login import (
+    login_required,
+    login_user,
+    logout_user,
+    current_user,
+)
 
 @main.route('/')
 @main.route('/index/')
 def index():
-    return render_template('index.html')
+    topics = Topic.query.order_by('-create_time').all()
+    return render_template('index.html', topics=topics)
 
 
-@main.route('/detail/')
-def detail():
-    return render_template('detail.html')
+@main.route('/<int:id>')
+def detail(id):
+    topic_model = Topic.id
+    return render_template('detail.html', topic=topic_model)
+
+
+@main.route('/publish/', methods=['GET','POST'])
+@login_required
+def publish():
+    form = request.form
+    if request.method == 'POST':
+        topic = Topic(title=form.get('title'),
+                      body=form.get('body'),
+                      author=current_user._get_current_object())
+        db.session.add(topic)
+        db.session.commit()
+        flash('发布成功')
+        return redirect(url_for('.index'))
+    return render_template('publish.html', form=form)
