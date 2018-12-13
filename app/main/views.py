0f03303af685f6c017d597ad1187622ca8f9a605
AngusZHAN <573585136@qq.com>
2018-12-13 16:43:34 +0800

    写好了话题修改和用户资料的后台功能

--- app/main/views.py
+++ app/main/views.py
@@ -30,7 +30,7 @@ def index():
 @main.route('/detail/<topic_id>')
 def detail(topic_id):
     topic_model = Topic.query.filter(Topic.id == topic_id).first()
-    replies = Reply.query.filter(topic_id == topic_id).all()
+    replies = Reply.query.filter_by(topic_id = topic_id).all()
     return render_template('detail.html', topic=topic_model, replies=replies)
 
 
@@ -62,3 +62,48 @@ def reply(topic_id):
         flash('回复成功')
         return redirect(url_for('.detail', topic_id=topic_id))
     return render_template('reply.html', form=form, topic_id=topic_id)
+
+
+@main.route('/edit/<int:id>', methods=['GET', 'POST'])
+@login_required
+def edit(id):
+    topic = Topic.query.get_or_404(id)
+    if current_user != topic.author:
+        flash('您不是话题作者，无法修改。')
+        return redirect(url_for('.detail', topic_id=id))
+    form = request.form
+    if request.method == 'POST':
+        topic = Topic(body=form.get('body'))
+        db.session.add(topic)
+        db.session.commit()
+        flash('文章修改成功')
+        return redirect(url_for('.detail', topci_id=Topic.id))
+    Topic(body=topic.body)
+    return render_template('edit_topic.html', form=form)
+
+
+@main.route('/user/<username>')
+def user(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        abort(404)
+    # posts = user.posts.order_by().all
+    return render_template('user.html', user=user)
+
+
+@main.route('/edit_profile/', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = request.form
+    if request.method == 'POST' and form.validate():
+        current_user.name = form.get(name)
+        current_user.location = form.get(location)
+        current_user.about_me = form.get(about_me)
+        db.session.add(current_user)
+        db.session.commit()
+        flash('修改资料成功')
+        return redirect(url_for('.user', username=current_user.username))
+    form.name.data = current_user.name
+    form.location.data = current_user.location
+    form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', form=form, user=user)
